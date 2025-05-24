158bb0f89dddb47e7f48949fb00e4d9fa4471aec
Oscar Lezcano <oscar.lezcano@fiuni.edu.py>
2025-05-24 19:27:14 -0300

    Me olvide quitar ese comentario

--- src/utils/file_handler.py
+++ src/utils/file_handler.py
@@ -3,21 +3,6 @@ import json
 class FileHandler:
     """
     Una clase de utilidad para manejar operaciones con archivos JSON.
-    Métodos:
-        read_json(file_path: str) -> dict:
-            Lee un archivo JSON desde la ruta especificada y devuelve su contenido como un diccionario.
-            Args:
-                file_path (str): La ruta del archivo JSON a leer.
-            Returns:
-                dict: El contenido del archivo JSON.
-                
-        write_json(file_path: str, data: dict) -> None:
-            Escribe un diccionario en un archivo JSON en la ruta especificada.
-            Args:
-                file_path (str): La ruta del archivo JSON a escribir.
-                data (dict): Los datos del diccionario a escribir en el archivo JSON.
-            Returns:
-                None
     """
 
     @staticmethod
